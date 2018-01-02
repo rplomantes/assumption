1b6ec5e83c282ba7113655cc5cfa81013d9215ae
royplomantes <royplomantes@192.168.1.9>
2018-01-02 15:19:58 +0800

    2018-01-02 C

--- app/Http/Controllers/RegistrarCollege/Assessment/AssessmentController.php
+++ app/Http/Controllers/RegistrarCollege/Assessment/AssessmentController.php
@@ -320,6 +320,8 @@ class AssessmentController extends Controller {
             $changestatus=  \App\Status::where('idno',$idno)->first();
             $changestatus->status=env("ENROLLED");
             $changestatus->update();
+            $checkreservations->is_consumed = '1';
+            $checkreservations->update();
             MainPayment::addUnrealizedEntry($request, $reference_id);
             MainPayment::processAccounting($request, $reference_id, $totalpayment, $ledgers,env("DEBIT_MEMO"));
             $this->postDebit($idno, $reference_id, $totalpayment);
