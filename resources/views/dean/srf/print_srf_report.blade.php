3a1e6720f6970ef252358acf6f910ec37e42bb4e
nephila <nephila@nephila-lenovo-ideapad-320>
2018-02-05 10:30:09 +0800

    02-05-2018 -S srf report

--- resources/views/dean/srf/print_srf_report.blade.php
+++ resources/views/dean/srf/print_srf_report.blade.php
@@ -17,11 +17,29 @@
     <div style='float: left; margin-top:12px; margin-left: 10px' align='center'><span id="schoolname">Assumption College</span> <br><small> San Lorenzo Drive, San Lorenzo Village<br> Makati City</small><br><br><b>Subject Related Fee</b></div>
 </div>
 <div>
-    <table class='table' border="1" width="100%" cellspacing='0' cellpadding='0' style='margin-top: 145px;'>
+    <center style='margin-top: 135px;'>
+        <b>{{$program_name}}</b><br>
+        Curriculum Year: <u>{{$curriculum_year}}</u><br>
+        {{$level}} - {{$period}}
+    </center>
+    <table class='table' border="1" width="100%" cellspacing='0' cellpadding='5' style='margin-top: 20px;'>
        <tr>
-           <th></th>
-           <th>{{$program_name}}</th>
-           <th>
+           <th>Code</th>
+           <th>Subject Description</th>
+           <th><div align="center">SRF</div></th>
+       </tr>
+       <?php $totalsrf = 0; ?>
+       @foreach ($programs as $program)
+       <?php $totalsrf = $totalsrf + $program->srf; ?>
+       <tr>
+           <td>{{$program->course_code}}</td>
+           <td>{{$program->course_name}}</td>
+           <td>{{$program->srf}}</td>
+       </tr>
+       @endforeach
+       <tr>
+           <td colspan="2"><b>Total</b></td>
+           <td><b>{{number_format($totalsrf,2)}}</b></td>
        </tr>
    </table>
 </div>
