6c16bdff818c7efa30cbbdffe08cf9778e367643
Sean Mervin Ronquillo <mervinronquillo@gmail.com>
2018-07-02 14:32:52 +0800

    update 07-2-2018 -Ronky reformat ched enrollment report

--- resources/views/reg_college/reports/print_ched_enrollment_report.blade.php
+++ resources/views/reg_college/reports/print_ched_enrollment_report.blade.php
@@ -17,23 +17,52 @@
 
 </style>
 <style>
+    @page {
+                margin: 1cm 1cm;
+            }
     body{
         font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
         font-size: 9pt;
+                margin-top: 6.2cm;
+                margin-left: 0cm;
+                margin-right: 0cm;
+                margin-bottom: 2.5cm;
     }
     
     header {
         position: fixed; 
-        bottom: 6cm; 
+        top: 0cm; 
         left: 0px; 
         right: 0px;
         height: 0px; 
 
-        margin: 0cm 1cm cm 1cm;
+        margin: 0cm 0cm cm 0cm;
+
+    }
+    footer {
+        position: fixed; 
+        bottom: 3cm; 
+        left: 0px; 
+        right: 0px;
+        height: 0px; 
+
+        margin: 0cm 0cm cm 0cm;
 
     }
 </style>
 <body>
+    
+    <script type="text/php">
+        if ( isset($pdf) ) {
+            $x = 700;
+            $y = 110;
+            $text = "Page {PAGE_NUM} of {PAGE_COUNT}";
+            $font = $fontMetrics->get_font("helvetica");
+            $size = 7;
+            $pdf->page_text($x, $y, $text, $font, $size);
+        }
+    </script>
+    <header>
     <div style='float: left; margin-left: 270px;'><img src="{{public_path('/images/assumption-logo.png')}}"></div>
     <!--<div style='float: left; margin-top:12px; margin-left: 10px' align='center'><span id="schoolname">Assumption College</span> <br><small> San Lorenzo Drive, San Lorenzo Village<br> Makati City</small><br><br><b>CHED ENROLLMENT REPORTS</b><br></div>-->    
     <div style='float: left; margin-top:12px; margin-left: 10px' align='center'><span id="schoolname">Assumption College</span> <br><small> San Lorenzo Drive, San Lorenzo Village<br> Makati City</small><br><br>OFFICE OF THE REGISTRAR<br><b>CHED ENROLLMENT LIST REPORT</b><br><b>{{$request->period}}, {{$request->school_year}} - {{$request->school_year + 1}}</b></div>
@@ -41,24 +70,41 @@
     <table  style='margin-top: 135px;' width="100%">
         <thead>
             <tr>
-                <td width="15%"><b>Institutional Identifier:</b><td><b>13022</b></td>
+                <td width="15%"><b>Institutional Identifier:</b></td><td width="50%"><b>13022</b></td>
+                <td><b>Semester/Trimester:</b></td><td><b>{{$request->period}}</td>
             </tr>
             <tr>
                 <td><b>Name of Institution:</b></td><td><b>Assumption College</b></td>
+                <td><b>Year Level:</b></td><td><b>{{$request->level}}</b></td>
             </tr>
             <tr>
-                <td><b>Address:</b></td><td><b>San Lorenzo Village, Makati City</b></td>
+                <td><b>Address:</b></td><td><b>San Lorenzo Village, Makati City</b></td><td></td><td></td>
             </tr>
             <tr>
-                <td><b>Program:</b></td><td><b><?php $program_name = \App\CtrAcademicProgram::where('program_code', $request->program_code)->first()->program_name; ?>{{$program_name}}</b></td>
+                <td><b>Program:</b></td><td><b><?php $program_name = \App\CtrAcademicProgram::where('program_code', $request->program_code)->first()->program_name; ?>{{$program_name}}</b></td><td></td><td></td>
             </tr>
             <tr>
-                <td><b>Tel. No.:</b></td><td><b>817-0757 loc. 2030</b></td>
+                <td><b>Tel. No.:</b></td><td><b>817-0757 loc. 2030</b></td><td></td><td></td>
             </tr>
         </thead>
     </table>
+    </header>
     
-    <table class='table' width="100%" cellspacing='0' cellpadding='0' style='margin-top:25px;'>
+    <footer>
+    <table width="100%">
+        <tbody>
+            <tr>
+                <td><b>Certified Correct:<br><br><br><br></b>
+                    <b>ROSIE B. SOMERA</b><br>
+                    Registrar<br>
+
+                    <div align="right">Date Printed: {{ date('Y-m-d H:i:s') }}</div></td>
+            </tr>
+        </tbody>
+    </table>
+    </footer>
+    
+    <table class='table' width="100%" cellspacing='0' cellpadding='0' style='margin-top:0px;'>
         <thead>
             <tr>
                 <th style="border-top: 1pt dotted black;border-bottom: 1pt dotted black"><?php $counter = 1; ?></th>
@@ -109,18 +155,6 @@
             <tr>
                 <td colspan="8"><b>Total Number of Students: {{count($students)}}</b></td>
             </tr>                    
-        </tbody>
-    </table>
-    <br><br><br>
-    <table width="100%">
-        <tbody>
-            <tr>
-                <td><b>Certified Correct:<br><br><br><br></b>
-                    <b>ROSIE B. SOMERA</b><br>
-                    Registrar<br>
-
-                    <div align="right">Date Printed: {{ date('Y-m-d H:i:s') }}</div></td>
-            </tr>
         </tbody>
     </table>
 </body>
