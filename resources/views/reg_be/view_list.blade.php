2c16fa0079e280450ce35a8a0bca4c5629080047
Sean Mervin Ronquillo <mervinronquillo@gmail.com>
2018-07-27 11:27:33 +0800

    update 07-27-2018 -Ronky update in reg be student list capitalization

--- resources/views/reg_be/view_list.blade.php
+++ resources/views/reg_be/view_list.blade.php
@@ -1,7 +1,7 @@
 <?php
 function get_name($idno){
     $names = \App\User::where('idno',$idno)->first();
-    return strtoupper($names->lastname).", ".$names->firstname." (".$names->middlename.")";
+    return strtoupper($names->lastname).", ".ucwords($names->firstname)." (".ucwords($names->middlename).")";
 }
 function get_ns($idno){
     $is_new = \App\BedLevel::where('idno',$idno)->first();
