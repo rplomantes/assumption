ac9a40ae696e741e331bf29d05ad017c0b21ca25
Sean Mervin Ronquillo <mervinronquillo@gmail.com>
2019-05-20 10:05:37 +0800

    May 20,  2019 -S update nstp report

--- resources/views/reg_college/reports/print_nstp_reports.blade.php
+++ resources/views/reg_college/reports/print_nstp_reports.blade.php
@@ -1,4 +1,12 @@
+<?php
+function getAge($dob,$condate){ 
+            $birthdate = new DateTime(date("Y-m-d",  strtotime($dob)));
+            $today= new DateTime(date("Y-m-d",  strtotime($condate)));           
+            $age = $birthdate->diff($today)->y;
+            return $age;
 
+}
+?>
 <style>
     img {
         display: block;
@@ -130,14 +138,17 @@
     <table class='table' border="1" width="100%">
         <thead>
             <tr>
-                <th align='center' width='1%'>#</th>
-                <th align='center' width='2%'>ID</th>
-                <th align='center' width='20%'>Student Name</th>
-                <th align='center' width='25%'>Course</th>
-                <th align='center' width='25%'>Address</th>
-                <th align='center' width='4%'>Birthdate</th>
-                <th align='center' width='3%'>Gender</th>
-                <th align='center' width='5%'>Tel No.</th>
+                <th rowspan="2" align='center' width='1%'>#</th>
+                <th rowspan="2" align='center' width='5%'>SERIAL NO.</th>
+                <th align='center' width='20%' colspan="3">NAME</th>
+                <th rowspan="2" align='center' width='25%'>COURSE</th>
+                <th rowspan="2" align='center' width='4%'>DATE OF BIRTH</th>
+                <th rowspan="2" align='center' width='5%'>AGE</th>
+            </tr>
+            <tr>
+                <th>FAMILY NAME</th>
+                <th>FIRST NAME</th>
+                <th>MIDDLE NAME</th>
             </tr>
         </thead>
         
@@ -148,17 +159,28 @@
             <?php $info = \App\StudentInfo::where('idno', $student->idno)->first(); ?>
             <?php $count = $count +1?>
             <tr>
-                <td>{{$count}}.</td>
-                <td>{{$user->idno}}</td>
-                <td>{{strtoupper($user->lastname)}}, {{strtoupper($user->firstname)}} {{strtoupper($user->middlename)}}</td>
+                <td align="right">{{$count}}.</td>
+                <td></td>
+                <td>{{strtoupper($user->lastname)}}</td>
+                <td>{{strtoupper($user->firstname)}}</td>
+                <td>{{strtoupper($user->middlename)}}</td>
                 <td>{{strtoupper($info->program_name)}}</td>
-                <td>{{$info->street}}, {{$info->barangay}},{{$info->municipality}},{{$info->province}}</td>
                 <td align='center'>{{date('m/d/Y',strtotime($info->birthdate))}}</td>
-                <td align='center'>F</td>
-                <td align='center'>{{$info->tel_no}}</td>
+                <td align='center'>{{getAge($info->birthdate, date('Y-m-d'))}}</td>
             </tr>
             @endforeach
         </tbody>
+        <tfoot>
+            <tr>
+                <th rowspan="2"></th>
+                <th rowspan="2">Total</th>
+                <th>Male:</th>
+                <th rowspan="2" colspan="5" style="border-bottom: none; border-right: none"></th>
+            </tr>
+            <tr>
+                <th>Female: {{$count}}</th>
+            </tr>
+        </tfoot>
     </table>
     <br>
     <table width="100%">
