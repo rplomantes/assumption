f96f7622e248c18027648e41e0dc7a243182e3e3
Sean Mervin Ronquillo <mervinronquillo@gmail.com>
2019-09-11 17:35:51 +0800

    September 11, 2019 -S audit student report

--- routes/web3.php
+++ routes/web3.php
@@ -16,6 +16,10 @@ Route::post('/registrar_college/reports/print_transfer_student','RegistrarColleg
 Route::get('/registrar_college/reports/list_foreign_student','RegistrarCollege\Reports\ListForeignStudentController@index');
 Route::post('/registrar_college/reports/print_foreign_student','RegistrarCollege\Reports\ListForeignStudentController@print_foreign_student');
 
+//List Audit Student
+Route::get('/registrar_college/reports/list_audit_student','RegistrarCollege\Reports\ListAuditStudentController@index');
+Route::post('/registrar_college/reports/print_audit_student','RegistrarCollege\Reports\ListAuditStudentController@print_audit_student');
+
 //List Unofficially Enrolled Student    
 Route::get('/registrar_college/reports/list_unofficially_enrolled', 'RegistrarCollege\Reports\ListUnofficiallyEnrolledController@index');
 Route::post('/registrar_college/reports/print_unofficially_enrolled', 'RegistrarCollege\Reports\ListUnofficiallyEnrolledController@print_unofficial');
