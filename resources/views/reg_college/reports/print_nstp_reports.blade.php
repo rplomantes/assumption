834b971ff911b4179ba193722923a0f0ea33cef7
Sean Mervin Ronquillo <mervinronquillo@gmail.com>
2019-10-08 09:45:08 +0800

    October 8, 2019 -S

--- resources/views/reg_college/reports/print_nstp_reports.blade.php
+++ resources/views/reg_college/reports/print_nstp_reports.blade.php
@@ -142,11 +142,11 @@ function getAge($dob,$condate){
                 <!--<th rowspan="2" align='center' width='5%'>SERIAL NO.</th>-->
                 <th align='center' width='5%'>ID NUMBER</th>
                 <th align='center' width='15%'>STUDENT NAME</th>
-                <th align='center' width='20%'>COURSE</th>
+                <th align='center' width='25%'>COURSE</th>
                 <th align='center' width='25%'>ADDRESS</th>
                 <th align='center' width='4%'>DATE OF BIRTH</th>
                 <!--<th align='center' width='5%'>AGE</th>-->
-                <th align='center' width='5%'>GENDER</th>
+                <th align='center' width='4%'>GENDER</th>
                 <th align='center' width='5%'>TEL. NO</th>
             </tr>
         </thead>
