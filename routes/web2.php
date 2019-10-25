53ca7f8ccdb8b48bc4576eef3da3e9ad44dc627b
Sean Mervin Ronquillo <mervinronquillo@gmail.com>
2019-10-25 16:12:22 +0800

    October 25, 2019 -S debug in Set up list ajax controller

--- routes/web2.php
+++ routes/web2.php
@@ -53,7 +53,7 @@ Route::get('/cashier/deposit_slip/{transaction_date}','Cashier\CollectionReport@
 Route::post('/cashier/deposit_slip','Cashier\CollectionReport@post_deposit_slip');
 Route::get('/cashier/remove_deposit/{id}','Cashier\CollectionReport@remove_deposit');
 Route::get('/cashier/printreceipt/{reference_id}', 'Cashier\PrintController@printreceipt');
-Route::get('/cashier/print_collection_report/{datefrom}/{dateto}','Cashier\PrintController@print_collection_report');
+Route::get('/cashier/print_collection_report/{datefrom}/{dateto}/{posted_by}','Cashier\PrintController@print_collection_report');
 
 //Pre Registration Payment//////////////////////////////////////////////////////
 Route::get('/cashier/pre_registration_payment','Cashier\PreRegistration@pre_registration_payment');
