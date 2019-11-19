22ad940e5277d3ae3077bd5b4f6ef46bb9949f97
Sean Mervin Ronquillo <mervinronquillo@gmail.com>
2019-11-19 14:13:36 +0800

    November 19, 2019 -S update in bed admissions

--- resources/views/accounting/ajax/getplan.blade.php
+++ resources/views/accounting/ajax/getplan.blade.php
@@ -394,9 +394,7 @@ $grade2total = $grade2tuition+$grade2misc+$grade2others+$grade2dep;
     <tr><td>ABM</td><td align="right">{{get_srf('Grade 11','ABM')}}</td><td align="right">{{get_srf('Grade 12','ABM')}}</td></tr>
     <tr><td>HUMMS</td><td align="right">{{get_srf('Grade 11','HUMSS')}}</td><td align="right">{{get_srf('Grade 12','HUMSS')}}</td></tr>
     <tr><td>STEM</td><td align="right">{{get_srf('Grade 11','STEM')}}</td><td align="right">{{get_srf('Grade 12','STEM')}}</td></tr>
-    <tr><td>GA</td><td align="right">{{get_srf('Grade 11','GA')}}</td><td align="right">{{get_srf('Grade 12','GA')}}</td></tr>
-    <tr><td>PA</td><td align="right">{{get_srf('Grade 11','PA')}}</td><td align="right">{{get_srf('Grade 12','PA')}}</td></tr>
-    <tr><td>VA</td><td align="right">{{get_srf('Grade 11','VA')}}</td><td align="right">{{get_srf('Grade 12','VA')}}</td></tr>
+    <tr><td>AD</td><td align="right">{{get_srf('Grade 11','PA')}}</td><td align="right">{{get_srf('Grade 12','PA')}}</td></tr>
     <tr><td>Total SRF</td><td align="right">{{get_total('Grade 11')}}</td><td align="right">{{get_total('Grade 12')}}</td></tr>
 </table>    
 </div>
@@ -425,4 +423,5 @@ $grade2total = $grade2tuition+$grade2misc+$grade2others+$grade2dep;
   
  </table>
 
+@endif
 <a href="{{url('/accounting',array('ajax','print_getplan',$department))}}" class="btn btn-primary" target="_blank">Print Plan</a>
