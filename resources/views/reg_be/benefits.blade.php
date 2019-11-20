eb08cb08032a6e5a70a6bf899452807c436b4bbe
Sean Mervin Ronquillo <mervinronquillo@gmail.com>
2019-11-20 12:00:21 +0800

    November 20, 2019 -S

--- resources/views/reg_be/benefits.blade.php
+++ resources/views/reg_be/benefits.blade.php
@@ -84,7 +84,7 @@
                         </td>
                         <td>@if(count($is_sibling)>0) <span class="fa fa-check"></span> @else  @endif</td>
                         <?php $partial_discount = \App\PartialStudentDiscount::where('idno',$sibling->idno)->first(); ?>
-                        <td>{{$partial_discount->discount}}%</td>
+                        <td>@if(count($partial_discount)>0){{$partial_discount->discount}}%@endif</td>
                         <td align="right"><a href="{{url("/remove_benefit", array($sibling->idno))}}">Remove Student</a></td>
                     </tr>
                     @endforeach
