5190c8cbbcae295c487b4e11dfbda73f358a4945
Sean Mervin Ronquillo <mervinronquillo@gmail.com>
2019-12-03 09:54:27 +0800

    Dec 3, 2019 -S

--- resources/views/reg_be/ajax/getstudentlist.blade.php
+++ resources/views/reg_be/ajax/getstudentlist.blade.php
@@ -4,7 +4,7 @@
     @foreach($lists as $list)
     <?php $status = \App\Status::where('idno', $list->idno)->first(); ?>
     @if($list->accesslevel == '0' && $list->academic_type=="BED" && $status->status<=4 || $list->academic_type=="SHS")
-    <tr><td>{{$list->idno}}</td><td>{{$list->lastname}}, {{$list->firstname}}</td>
+    <tr><td>{{$list->idno}}</td><td>{{$list->lastname}}, {{$list->firstname}} {{$list->middlename}}</td>
         <td>
             @if($status->status == 3)Enrolled
             @elseif($status->status == 2) Assessed
