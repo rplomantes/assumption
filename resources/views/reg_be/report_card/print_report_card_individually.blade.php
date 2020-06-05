5cb021db5cd8ea9dcaf262bd14b305d3cadcb8b5
Sean Mervin Ronquillo <mervinronquillo@gmail.com>
2020-06-05 10:43:46 +0800

    June 5, 2020 report card

--- resources/views/reg_be/report_card/print_report_card_individually.blade.php
+++ resources/views/reg_be/report_card/print_report_card_individually.blade.php
@@ -225,8 +225,12 @@ function getPromotion($level) {
 
         @endif
 
-        @if($subject->final_grade != "")
+        @if($subject->final_grade != "" or $subject->final_remarks != "")
+        @if(!fnmatch("SA[1,2,3,4,5,6,7,8,9,10]", $subject->group_code))
+        <td align="center">{{$subject->final_remarks}}</td>
+        @else
         <td align="center">{{$subject->final_remarks}}({{$subject->final_grade}})</td>
+        @endif
         @if($status->level == "Grade 7" || $status->level == "Grade 8" || $status->level == "Grade 9" || $status->level == "Grade 10")
         <td align="center">Promoted</td>
         @endif
