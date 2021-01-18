527a0e6ec8e5415319ad9c67f5a4884c904f327e
sean <sean@Sean-R-Ideapad-S145>
2021-01-18 15:26:10 +0800

    payment plan report

--- resources/views/layouts/appaccountingstaff.blade.php
+++ resources/views/layouts/appaccountingstaff.blade.php
@@ -228,6 +228,7 @@ $date_end = date('Y-m-d');
                                 <li><a href="{{url('/accounting',array('sibling_discount'))}}"><span>Sibling Discounts</span></a></li>
                                 <li><a href="{{url('/accounting',array('realized_fees'))}}"><span>*Realized Fees</span></a></li>
                                 <li><a href="{{url('/accounting',array('payment_summary'))}}"><span>*Payment Summary</span></a></li>
+                                <li><a href="{{url('/accounting',array('payment_plans'))}}"><span>Payment Plans</span></a></li>
                                 <li><a href="{{url('/accounting',array('nstp_report'))}}"><span>NSTP Report</span></a></li>
                             </ul>
                         </li>       
