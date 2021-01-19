d175bceffd3a87e4853be3c61081ce7a342b426b
sean <sean@Sean-R-Ideapad-S145>
2021-01-19 14:19:25 +0800

    paynamic settings

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,20 +8,19 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class LoginController extends Controller
-{
+class LoginController extends Controller {
     /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+      |--------------------------------------------------------------------------
+      | Login Controller
+      |--------------------------------------------------------------------------
+      |
+      | This controller handles authenticating users for the application and
+      | redirecting them to your home screen. The controller uses a trait
+      | to conveniently provide its functionality to your applications.
+      |
+     */
 
-    use AuthenticatesUsers;
+use AuthenticatesUsers;
 
     /**
      * Where to redirect users after login.
@@ -35,64 +34,81 @@ class LoginController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
 //        $this->addadmin();
 //        $this->add999999();
 //        $this->updateLedger();
         //$this->generatePassword();
+        $this->updateTransactionDate();
         $this->middleware('guest')->except('logout');
     }
-    
+
     public function username() {
         return 'idno';
     }
-    
-    public function login (Request $request) {
-        
+
+    public function login(Request $request) {
+
         $this->validateLogin($request);
-        
+
         if (Auth::attempt(['idno' => $request->input('idno'), 'password' => $request->input('password'), 'status' => 1])) {
             return redirect()->intended('/');
         } else if (Auth::attempt(['idno' => $request->input('idno'), 'password' => $request->input('password'), 'status' => 0])) {
-                Auth::logout();
-                return view('auth.login')->withErrors("Access Denied - Account Deactivated");
+            Auth::logout();
+            return view('auth.login')->withErrors("Access Denied - Account Deactivated");
         }
         return $this->sendFailedLoginResponse($request);
     }
-    
-    public function addadmin(){
+
+    public function addadmin() {
         $check = DB::Select("Select * from users where idno = 'admin'");
-        if(count($check)<=0){
+        if (count($check) <= 0) {
             $password = bcrypt("nephilaadmin");
             DB::Select("Insert into users(idno,lastname,firstname,accesslevel,email, password) values('admin','admin','admin','100','admin@yahoo.com','$password')");
             DB::Select("Insert into users(idno,lastname,firstname,accesslevel,email, password) values('999999','none','none','0','999999@999999.com',NULL)");
         }
     }
-    public function add999999(){
+
+    public function add999999() {
         $check = DB::Select("Select * from users where idno = 999999");
-        if(count($check)<=0){
+        if (count($check) <= 0) {
             $password = bcrypt("999999");
             DB::Select("Insert into users(idno,lastname,firstname,accesslevel,email, password) values('999999','none','none','0','999999@999999.com','$password')");
         }
     }
-    public function generatePassword(){
+
+    public function generatePassword() {
         $prof = \App\User::where('accesslevel', 1)->where('is_first_login', 1)->get();
-        foreach ($prof as $profs){
+        foreach ($prof as $profs) {
             $password = strtolower($profs->lastname);
             $pass = bcrypt($password);
             $profs->password = "$pass";
             $profs->save();
         }
     }
-    public function updateLedger(){
-        $prof = \App\CollegeLevel::where('status','>',2)->where('school_year', 2019)->get();
-        foreach ($prof as $profs){
+
+    public function updateLedger() {
+        $prof = \App\CollegeLevel::where('status', '>', 2)->where('school_year', 2019)->get();
+        foreach ($prof as $profs) {
             $ledgers = \App\Ledger::where('idno', $profs->idno)->where('school_year', 2019)->get();
-            foreach ($ledgers as $ledger){
+            foreach ($ledgers as $ledger) {
                 $ledger->level = $profs->level;
                 $ledger->save();
             }
         }
     }
+
+    function updateTransactionDate() {
+        $gettransactions = \App\Accounting::distinct()->where('transaction_date', null)->get(['reference_id', 'transaction_date']);
+        foreach ($gettransactions as $transaction) {
+            $getdate = \App\Accounting::where('reference_id', $transaction->reference_id)->where('transaction_date', '!=',null)->first();
+            $gets = \App\Accounting::where('reference_id', $transaction->reference_id)->where('transaction_date', null)->get();
+            foreach ($gets as $get) {
+                $get->transaction_date = $getdate->transaction_date;
+                $get->update();
+            }
+        }
+        return "Done";
+    }
+
 }
