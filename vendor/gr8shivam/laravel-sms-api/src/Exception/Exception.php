9e4317726ddba29cd3f02bba02403673ce04b5d4
sean <jeff.lapuz09@gmail.com>
2021-01-25 15:44:06 +0800

    untrack vendor directory

--- vendor/gr8shivam/laravel-sms-api/src/Exception/Exception.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Gr8Shivam\SmsApi\Exception;
-
-class Exception extends \Exception {
-
-}
--- vendor/gr8shivam/laravel-sms-api/src/Exception/InvalidMethodException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Gr8Shivam\SmsApi\Exception;
-
-class InvalidMethodException extends Exception {
-
-}
