e87a546c31cc80521ed18db6bf499fd4ad83cadb
sean <jeff.lapuz09@gmail.com>
2021-01-25 15:19:14 +0800

    test

--- resources/views/reg_be/assess.blade.php
+++ resources/views/reg_be/assess.blade.php
@@ -2,7 +2,6 @@
 $promotion = \App\Promotion::where('idno', $user->idno)->first();
 $enrollment_sy = \App\CtrEnrollmentSchoolYear::where('academic_type', $status->academic_type)->first();
 $current_level="";
-
 $levels = \App\CtrAcademicProgram::selectRaw("distinct level, sort_by")->where('academic_type',"BED")->orderBy('sort_by')->get();
 $strands =\App\CtrAcademicProgram::selectRaw("distinct strand")->where('academic_code','SHS')->get();
 switch ($status->level){
