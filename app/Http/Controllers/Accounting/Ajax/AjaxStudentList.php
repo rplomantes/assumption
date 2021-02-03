ad5f0e79d972b15ace2ec528f2235743b38e3c80
sean <mervinronquill@gmail.com>
2021-02-03 14:52:55 +0800

    bug in assessment report

--- app/Http/Controllers/Accounting/Ajax/AjaxStudentList.php
+++ app/Http/Controllers/Accounting/Ajax/AjaxStudentList.php
@@ -40,15 +40,17 @@ class AjaxStudentList extends Controller {
     function getSRF($lists, $department,$school_year,$period) {
         foreach ($lists as $list) {
             if ($department == "College Department") {
-                $lists_srf = DB::select("SELECT SUM(amount) AS assessment FROM ledgers WHERE category_switch IN (4,14) AND category = 'SRF' AND school_year = $school_year AND period = '$period' and idno = $list->idno");
+                $lists_srf = DB::select("SELECT SUM(amount) AS assessment FROM ledgers WHERE category_switch IN (4,14) AND category = 'SRF' AND school_year = $school_year AND period = '$period' and idno = '$list->idno'");
                 if (count($lists_srf) > 0) {
                     foreach ($lists_srf as $list_srf) {
                         $srf_amount = $list_srf->assessment;
                     }
+                }else{
+                    $srf_amount = 0;
                 }
                 $list->srf_amount = $srf_amount;
             } else if($department == "Senior High School") {
-                $lists_srf = DB::select("SELECT SUM(amount) AS assessment FROM ledgers WHERE category_switch IN (4,14) AND category = 'SRF' AND school_year = $school_year AND period = '$period' and idno = $list->idno");
+                $lists_srf = DB::select("SELECT SUM(amount) AS assessment FROM ledgers WHERE category_switch IN (4,14) AND category = 'SRF' AND school_year = $school_year AND period = '$period' and idno = '$list->idno'");
                 if (count($lists_srf) > 0) {
                     foreach ($lists_srf as $list_srf) {
                         $srf_amount = $list_srf->assessment;
