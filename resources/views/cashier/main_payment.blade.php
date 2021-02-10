898f2c521767bb9f74b2dff2f8d35f68f0302ebb
sean <mervinronquill@gmail.com>
2021-02-10 14:47:54 +0800

    add parseFloat

--- resources/views/cashier/main_payment.blade.php
+++ resources/views/cashier/main_payment.blade.php
@@ -407,7 +407,7 @@ $previousarray = json_encode($previousarray);
         
         $("#previous_balance").on('keypress',function(e){
             if(e.keyCode==13){
-                var desiredpreviousfee = $(this).val();
+                var desiredpreviousfee = parseFloat($(this).val());
                 
                 if($("#previous_balance").val()==""){
                     $("#previous_balance").val("0.00");
