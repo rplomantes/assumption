17118c3130d44b851095d47286d37183674a9f49
sean <mervinronquill@gmail.com>
2021-02-16 13:51:00 +0800

    add remarks

--- app/Reservation.php
+++ app/Reservation.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     //
+    public function getRemarks(){
+        $remarks = \App\Payment::where('reference_id',  $this->reference_id)->first();
+        if(count($remarks)>0){
+        return $remarks->remarks;
+        }else{
+            return "";
+        }
+    }
 }
