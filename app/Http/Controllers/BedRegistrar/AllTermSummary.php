880e59687f36af6c37448edf81d76a471b6ffa54
sean <mervinronquill@gmail.com>
2021-05-07 12:05:04 +0800

    all term summary ee grades only

--- app/Http/Controllers/BedRegistrar/AllTermSummary.php
+++ app/Http/Controllers/BedRegistrar/AllTermSummary.php
@@ -22,16 +22,16 @@ class AllTermSummary extends Controller
         }
     }
 
-    function export_all_term_summary($level, $strand, $section, $school_year, $period = null) {
+    function export_all_term_summary($level, $strand, $section, $school_year, $is_ee,$period = null) {
         $lists = \App\Http\Controllers\BedRegistrar\Ajax\AllTermSummaryAjax::getListSubjectHeads($school_year, $level, $section, $period, $strand, 'lists');
         $subject_heads = \App\Http\Controllers\BedRegistrar\Ajax\AllTermSummaryAjax::getListSubjectHeads($school_year, $level, $section, $period, $strand, 'subject_heads');
         
         ob_end_clean();
-        Excel::create('All Term Summary', function($excel) use ($level, $strand, $section, $school_year,$period, $lists, $subject_heads) {
+        Excel::create('All Term Summary', function($excel) use ($level, $strand, $section, $school_year,$period, $lists, $subject_heads,$is_ee) {
             $excel->setTitle("All Term Summary");
 
-            $excel->sheet('All Term Summary', function ($sheet) use ($level, $strand, $section, $school_year, $period, $lists, $subject_heads) {
-                $sheet->loadView('reg_be.ajax.all_term_view_list', compact('school_year', 'level', 'section', 'period', 'strand', 'lists', 'subject_heads'));
+            $excel->sheet('All Term Summary', function ($sheet) use ($level, $strand, $section, $school_year, $period, $lists, $subject_heads,$is_ee) {
+                $sheet->loadView('reg_be.ajax.all_term_view_list', compact('school_year', 'level', 'section', 'period', 'strand', 'lists', 'subject_heads','is_ee'));
             });
         })->download('xlsx');
     }
